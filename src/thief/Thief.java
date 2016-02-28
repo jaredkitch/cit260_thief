11d3273de0739fbbd035cd476161cfe0db473cac
Jon <Jon@Jon-PC.attlocal.net>
2016-02-27 19:18:11 -0500

    Re-adding Test Cases

--- src/thief/Thief.java
+++ src/thief/Thief.java
@@ -41,15 +41,83 @@ public class Thief {
     
     
     public static void jonTests() {
-    
+        
+     Map mapSpotOne = new Map();
+         mapSpotOne.setX_axis(7);
+         mapSpotOne.setY_axis(2);
+        mapSpotOne.setZ_axis(0);
+         mapSpotOne.setAssociatedLocation(21);
+         System.out.println(mapSpotOne.toString());
+         
+         //Location class test cases
+         Location hallway = new Location();
+         hallway.setAvailableCommands(6);
+         hallway.setLocationNumber(21);
+         System.out.println(hallway.toString());
+         
+         //Stealabile Item Scene Class test cases
+         StealableItemScene vaseRoom = new StealableItemScene();
+         vaseRoom.setDescription("The vase sits behind a large glass case");
+         vaseRoom.setInteractables(7);
+         System.out.println(vaseRoom.toString());
+         
+         //Dummy Room Class test cases
+         DummyRoom lobby = new DummyRoom();
+         lobby.setDescription("This ornate room houses the Front desk and a set of stairs up");
+        System.out.println(lobby.toString());
+         
+         //Trap Scence Class test cases
+         TrapScene lasers = new TrapScene();
+         lasers.setDescription("Many lasers dance around the hallway as look across to see the diamond.");
+         lasers.setInteractables(3);
+         lasers.setTypeOfTrap("Laser");
+         System.out.println(lasers.toString());
+     
 }
     public static void jaredTests() {
+        Inventory inventory = new Inventory();
+         inventory.setSuppliesInInventory(5);
+         System.out.println(inventory.toString());
+          
+         Supplies supplies = new Supplies();
+         supplies.setNames("bomb");
+         supplies.setSupplyID(2);
+         System.out.println(supplies.toString());
+         
+         Van van = new Van();
+         van.setAvailableSupplies(2);
+         System.out.println(van.toString());
 }
     
     public static void tishaTests() {
+        Game game = new Game();
+         game.setTurnsRemaining(50);
+         System.out.println(game.toString());
+          
+         Guard guard = new Guard();
+         guard.setLocation(5);
+         guard.setSleepState("asleep");
+         System.out.println(guard.toString());
+     
+         StealableItems stealableItems = new StealableItems();
+         stealableItems.setName("Tish");
+         stealableItems.setValue(100000);
+         System.out.println(stealableItems.toString());
+         
+        StolenItems stolenItems = new StolenItems();
+         stolenItems.setTotalValue(500000);
+         stolenItems.setItemsStillInPlay("painting, vase, diamonds");
+         System.out.println(stolenItems.toString());
     }
     
     public static void groupTests() {
+        
+        Player player = new Player();
+         player.setName("Jon");
+         player.setLocation(8);
+         player.setFastestTime(30);
+         player.setBiggestHaul(5000);
+         System.out.println(player.toString());
     }
     
     
