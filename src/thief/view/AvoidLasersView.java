3a22f19f4dbd81d2f6fa82f84ad1ad9bd0f1aa83
tishparkinson <bak05010byui@gmail.com>
2016-03-05 15:15:49 -0700

    3/2 lesson8teamassignment

--- src/thief/view/AvoidLasersView.java
+++ src/thief/view/AvoidLasersView.java
@@ -5,14 +5,103 @@
  */
 package thief.view;
 
+import java.util.Scanner;
+import thief.control.TrapControl;
+
 /**
  *
  * @author Jon
  */
 public class AvoidLasersView {
 
-    void displayMenu() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private String menu;
+
+    
+    public AvoidLasersView() {
+
+    }
+
+    
+    public void displayMenu() {
+        
+        double done = 0; // set flag to not done
+        do {
+            // prompt for and get players name
+            menu = "\n.-------------------------------------------------------------------"
+                + "\n| Please Enter The Length Of The Laser Field"
+                + "\n|-------------------------------------------------------------------";
+            
+            int value1 = this.getValue();
+
+            menu = "\n.-------------------------------------------------------------------"
+                + "\n| Please Enter The Height Of The Laser Field"
+                + "\n|-------------------------------------------------------------------";            
+            
+            int value2 = this.getValue();
+            
+            menu = "\n.-------------------------------------------------------------------"
+                + "\n| Please Enter The Number Of Lasers In The Laser Field"
+                + "\n|-------------------------------------------------------------------"; 
+            
+            int value3 = this.getValue();
+            
+            // do the requested action and display the next view
+            done = this.doAction(value1, value2, value3);
+        } while (done == 0);
+        
+    }
+
+    private int getValue() {
+    Scanner keyboard = new Scanner(System.in); //keyboard input stream
+    int value = 0;
+    
+    boolean valid = false; //set flag to invalid value entered
+    while(!valid) { // while a valid name has not been retrieved
+        
+        //prompt for the player's name
+        System.out.println(this.menu);
+       
+        value = keyboard.nextInt(); //get the name from the keyboard
+
+        if (value >= 1000) { 
+            System.out.println(
+               "\n**********************************************************************"
+             + "\n***** Invalid value - the value cannot be more than Three digits *****"
+             + "\n**********************************************************************");
+            continue;
+           
+        } else if (value <= 0) {
+            System.out.println(
+                       "\n**************************************************************"
+                     + "\n***** Invalid value - the value cannot be less than Zero *****"
+                     + "\n**************************************************************");
+            continue;
+        }
+        
+        valid = true; // set flag to end repetition
+    }
+   return value; 
+
+        
+}
+    private double doAction(int number1, int number2, int number3) {
+ 
+        double keypad = TrapControl.keypadCombination(number1, number2, number3);
+        
+        if (keypad >= 0){
+        System.out.println( "\n.-------------------------------------------------------------------"
+                + "\n| The Keypad number is " + keypad + "                                          "
+                + "\n|-------------------------------------------------------------------"); 
+        return 7;
+        } else if (keypad == -911){
+        System.out.println( 
+                  "\n.-------------------------------------------------------------------------------"
+                + "\n| Something happened while you were calulating check your numbers and try again!"
+                + "\n|-------------------------------------------------------------------------------"); 
+        return 0;
+        }
+       return 0;
     }
     
 }
+
